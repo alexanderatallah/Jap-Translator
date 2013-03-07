116036540632df3a31d3e3fa1d785cfc997c5bc8
Alexander Atallah <aatallah@stanford.edu>
2013-03-06 23:44:04 -0800

    made basic translator

--- MachineTranslation.py
+++ MachineTranslation.py
@@ -1,9 +1,28 @@
 import sys, collections
 
+def initDicts(f):
+  d = {}
+  partsOfSpeech = {}
+  for line in f:
+    (f, e, pos) = line.split(',')
+    d[f] = e
+    partsOfSpeech[e] = pos
+  return (d, partsOfSpeech)
 
-def main:
-  language_to_english_dictionary = collections.defaultdict(lambda: 0)
-  
+def reorder(words):
+  return words
+
+def main():
+  d, p = initDicts(open("dictionary.txt"))
+  japanese = open("japanese_text_segmented.txt")
+  english = []
+  for line in japanese:
+    words = line.split("|")
+    for w in words:
+      if w in d: english.append(d[w])
+      else: english.append("NOT_IN_DICT:" + w)
+  reorder(english)
+  print " ".join(english)
 
 if __name__ == '__main__':
   main()
