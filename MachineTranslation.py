a3f0bd204926e23d2d90455a6b9d30de2d49b78e
Alexander Atallah <aatallah@stanford.edu>
2013-03-07 16:09:36 -0800

    made debug mode and added sentence splitting

--- MachineTranslation.py
+++ MachineTranslation.py
@@ -10,18 +10,36 @@ def initDicts(f):
   return (d, partsOfSpeech)
 
 def reorder(words, partsOfSpeech):
+  sentences = getSentences(words)
   newWords = []
-  for i in xrange(len(words)):
-    word = words[i]
-    prevWord = words[i-1] if i > 0 else ""
-    nextWord = words[i+1] if i < len(words) - 1 else ""
+  for sentence in sentences:
+
+    for i in xrange(len(sentence)):
+      word = sentence[i]
+      prevWord = sentence[i-1] if i > 0 else ""
+      nextWord = sentence[i+1] if i < len(sentence) - 1 else ""
+      newWords.append(word)
 
   return newWords
+
+def getSentences(words):
+  sentences = []
+  sentence = []
+  for word in words:
+    sentence.append(word)
+    if word == ".":
+      sentences.append(sentence[:])
+      sentence = []
+  return sentences
 
 def printEnglish(words, partsOfSpeech):
   output = ""
   for i in xrange(len(words)):
     word = words[i]
+    ######### COMMENT THIS OUT TO GET ACTUAL TRANSLATION ###########
+    ######### DEBUG MODE ###########
+    word += ":" + partsOfSpeech[word]
+    ######### END ############
     prevWord = words[i-1] if i > 0 else ""
     if prevWord == "." or prevWord == "":
       word = word[0].upper() + word[1:]
